dd8f327421a27d4e57208c7ac32f52f1ed6280b5
deanbrabec <brabec.dean@gmail.com>
2016-08-11 11:53:25 +0200

    Commit Test

--- wp-content/themes/uniqmag-premium-theme-child/includes/loop/post.php
+++ wp-content/themes/uniqmag-premium-theme-child/includes/loop/post.php
@@ -56,7 +56,7 @@
 
 
 	?>
-       <div class="<?php echo esc_attr($post_wrap_class); ?>">
+       <div class="<?php echo esc_attr($post_wrap_class); ?>">Tets
            
                                
                     <?php $terms = wp_get_post_terms( $post_id, $profession );
